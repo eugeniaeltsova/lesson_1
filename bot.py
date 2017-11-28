4d51482180105678ea154c788f6c30513fcf8493
eugeniaeltsova <jenya.eltsova@mail.ru>
2017-11-28 23:08:18 +0400

    virtual env added

--- bot.py
+++ bot.py
@@ -12,14 +12,49 @@ def greet_user (bot, update):
 
 def talk_to_me (bot, update):
     user_text = update.message.text # то, что ввел пользователь
-    logging.info (user_text)
-    update.message.reply_text(user_text)
+    logging.info (user_text) # сохранение в файл промежуточной информации
+    update.message.reply_text(user_text) #ответ бота
 
 
 def word_count(bot, update):
     phrase = update.message.text
     words_list = phrase.split(" ")
-    update.message.reply_text("Number of words is " + str(len(words_list)))
+  
+    update.message.reply_text( "Number of words is " + str(len(words_list) - 1))
+    if len(words_list) - 1 == 0:
+        update.message.reply_text("ну скажи что-нибудь")
+
+def calc(bot, update):
+    equation = update.message.text[6:]
+
+    if equation[-1] != "=":
+        update.message.reply_text("выражение должно иметь вид x + y = ")
+    equation = equation[: -1]
+    print (equation)
+
+    if "+" in equation:
+        equation_data = equation.split("+")
+        print (equation_data)
+        summ = int(equation_data[0]) + int(equation_data[1])
+        update.message.reply_text("сумма  равняется " + str(summ))
+    elif "-" in equation:
+        equation_data = equation.split("-")
+        subtraction = int(equation_data[0]) - int(equation_data[1])
+        update.message.reply_text("разность  равняется " + str(subtraction))
+    elif "/" in equation:
+        equation_data = equation.split("/")
+        division  = int(equation_data[0]) / int(equation_data[1])
+        update.message.reply_text("результат деления равняется " + str(division))
+    else:
+        equation_data = equation.split("*")
+        multiplication = int(equation_data[0]) * int(equation_data[1])
+        update.message.reply_text("результат умножения равняется " + str(multiplication))
+
+    
+
+
+
+
 
     # return print ("Number of words is " + str(len(words_list)))
 
@@ -28,10 +63,12 @@ def main ():
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(CommandHandler("wordcount", word_count))
+    dp.add_handler(CommandHandler("calc", calc))
     dp.add_handler (MessageHandler(Filters.text, talk_to_me))
 
     updater.start_polling()
     updater.idle()
+
    
 main ()  
 """def get_answer(statement):
